0c710e8e98e369797f4a60ee1386234eaef713eb
Mohamed Alaga <malaga.142014@gmail.com>
2024-12-14 18:07:29 +0200

    0. Regex-ing

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+""" Use of regex in replacing occurrences of certain field values """
 import re
 from operator import contains
 from typing import List
@@ -6,6 +7,7 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
+    """ Returns regex obfuscated log messages """
     for field in fields:
         message = re.sub(f'{field}=(.*?){separator}',
                          f'{field}={redaction}{separator}', message)
