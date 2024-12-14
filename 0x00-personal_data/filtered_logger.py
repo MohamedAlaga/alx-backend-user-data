a4dc2f1d38cfaac167552ce7c67ee54601e5aca8
Mohamed Alaga <malaga.142014@gmail.com>
2024-12-14 18:31:09 +0200

    1. Log formatter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -17,12 +17,19 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
+        """
+        constructor
+        """
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        textMessage = filter_datum(self.fields, self.REDACTION, super().format(record), self.SEPARATOR)
-        return textMessage
+        """
+        formating the log message
+        """
+        return filter_datum(self.fields,
+                            self.REDACTION, super().format(record),
+                            self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
